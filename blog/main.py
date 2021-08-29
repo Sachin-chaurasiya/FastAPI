75319179a8ddf267df4ac26749a77abe76d9f0e2
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-08-29 20:10:57 +0530

    changed table name for blog api

--- blog/main.py
+++ blog/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI,Depends,status,Response,HTTPException
 from . import schemas,models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
+
 tags_metadata = [
     {
-        "name": "blogs",
-        "description": "Blogs endpoints",
+        "name": "blog",
+        "description": "Blog endpoints",
     }
     ]
 app=FastAPI(title="Blog API",
@@ -21,7 +22,7 @@ def get_db():
     finally:
         db.close()    
 
-@app.post('/blog',status_code=status.HTTP_201_CREATED,tags=["blogs"])
+@app.post('/blog',status_code=status.HTTP_201_CREATED,tags=["blog"])
 def create(request:schemas.Blog,response:Response,db:Session=Depends(get_db)):
     new_blog=models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -29,7 +30,7 @@ def create(request:schemas.Blog,response:Response,db:Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}',status_code=status.HTTP_200_OK,tags=["blogs"])
+@app.delete('/blog/{id}',status_code=status.HTTP_200_OK,tags=["blog"])
 def destroy(id,db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
@@ -38,7 +39,7 @@ def destroy(id,db:Session=Depends(get_db)):
     db.commit()
     return {'details':"done"}
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=["blogs"])
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=["blog"])
 def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
@@ -49,17 +50,15 @@ def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
 
 
 
-@app.get('/blog',tags=["blogs"])
+@app.get('/blog',tags=["blog"])
 def all(db:Session=Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}',status_code=200,tags=["blogs"])
+@app.get('/blog/{id}',status_code=200,tags=["blog"])
 def show(id,response:Response, db:Session=Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id).first()  
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} is not available")
-        # response.status_code=status.HTTP_404_NOT_FOUND
-        # return {'detail':f"Blog with the id {id} is not available"}
     return blog
        
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from .database import Base
 from sqlalchemy import Column,Integer,String
 
 class Blog(Base):
-    __tablename__="blogs"
+    __tablename__="blog_entity"
     id=Column(Integer(),primary_key=True,index=True)
     title=Column(String)
     body=Column(String)
